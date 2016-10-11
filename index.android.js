fa317443b4865d372b1bfd8e04ce138acdca542a
Mathew Hall <mhmathewhall@gmail.com>
2016-10-11 03:05:22 +0100

    App positioning mostly done

--- index.android.js
+++ index.android.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
+  StyleSheet,
   AppRegistry
 } from "react-native";
 
 var StopWatch = React.createClass({
   render: function(){
-    return <View>
+    return <View style={styles.container}>
+          <View style={[styles.header, this.border("yellow")]}>
+            <View style={[styles.timerWrapper,this.border("red")]}>
               <Text>
                 00:00.00
               </Text>
-            {this.startStopButton()}
-            {this.lapButton()}
+            </View>
+            <View style={[styles.buttonWrapper, this.border("green")]}>
+              {this.startStopButton()}
+              {this.lapButton()}
+            </View>
+              </View>
+              <View style={[styles.footer, this.border("blue")]}>
+                <Text>List of laps</Text>
+              </View>
           </View>
   },
   startStopButton: function() {
@@ -28,6 +38,36 @@ var StopWatch = React.createClass({
         Lap
       </Text>
     </View>
+  },
+  border: function(color) {
+    return {
+      borderColor : color,
+      borderWidth: 4
+    }
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "stretch"
+  },
+  header: {
+    flex: 1
+  },
+  footer: {
+    flex: 1
+  },
+  timerWrapper:{
+    flex: 5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonWrapper: {
+    flex: 3,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
   }
 });
 
